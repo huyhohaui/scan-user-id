482285099c70fbe718a1d37a3b5bbeef655614b4
huyhohaui <b1.maitrongtoi@gmail.com>
2019-11-28 20:31:30 +0700

    up

---
+++ auto-add-fr.js
@@ -0,0 +1,55 @@
+// const scanUser = require('scan-user-id')
+const puppeteer = require("puppeteer")
+const CronJob = require('cron').CronJob;
+
+const {
+  convertCookieFacebook
+} = require("./cookie")
+
+async function clickAdd(page) {
+  await page.click('a[href*="/a/mobile/friends/profile_add_friend"]');
+
+  console.log('add success!')
+  console.log('AddFr Finish ...!')
+}
+
+async function main() {
+  const userId = ['100043842166914', '100011521398663', '100026295222252']
+  let urlUser = 'https://d.facebook.com/'
+  const cookie =
+    "sb=a6beXUxmABU9xS0KwkR3clpz; datr=bqbeXSqfBPU00FnuRWVrufnK; c_user=100005594357809; xs=37%3AgppwjlYVK9nNNQ%3A2%3A1574872721%3A5941%3A6351; fr=1CkYMcQ7QrlGloZLL.AWVp-p73xTlDw2MyWOUkofjMkg4.Bd3qZr.CE.AAA.0.0.Bd3qaR.AWUROxoR; spin=r.1001477064_b.trunk_t.1574872722_s.1_v.2_; wd=927x969; act=1574875261097%2F0; presence=EDvF3EtimeF1574875264EuserFA21B05594357809A2EstateFDt3F_5bDiFA2thread_3a1607618295944707A2EoF1EfF1C_5dEutc3F1574874678623G574875264955Elm3FnullCEchFDp_5f1B05594357809F1CC"
+
+  const browser = await puppeteer.launch({
+    headless: false
+  })
+  const page = await browser.newPage()
+  const cookieConverted = await convertCookieFacebook(cookie)
+  await page.setCookie(...cookieConverted)
+  await page.setViewport({
+    width: 1368,
+    height: 800
+  });
+  // await page.waitFor(1000);
+
+
+  for (index = 0; index < userId.length; index++) {
+    await page.goto(urlUser + userId[index], {
+      waitUntil: "networkidle2"
+    });
+
+
+    // error can't click when use CronJob
+    const clickAddFr = new CronJob('0 * * * * *', async () => {
+      await clickAdd(page)
+      console.log('test')
+    })
+    clickAddFr.start()
+
+    console.log('Added friend with id ' + userId[index])
+    // clickAddFr.stop()
+  }
+
+  console.log('Success ...!')
+}
+
+main()
---
+++ scan-user-id.js
@@ -0,0 +1,84 @@
+const puppeteer = require("puppeteer")
+const fetch = require("node-fetch")
+const random = require("random")
+
+const {
+  convertCookieFacebook
+} = require("./cookie");
+
+Object.defineProperty(Array.prototype, "flat", {
+  "value": function (depth = 1) {
+    return this.reduce(function (flat, toFlatten) {
+      return flat.concat((Array.isArray(toFlatten) && (depth > 1)) ? toFlatten.flat(depth - 1) : toFlatten);
+    }, []);
+  }
+});
+
+async function autoScroll(page) {
+  await page.evaluate(async () => {
+    await new Promise((resolve, reject) => {
+      var totalHeight = 0
+      var distance = 200
+      var timer = setInterval(() => {
+        var scrollHeight = document.body.scrollHeight;
+        window.scrollBy(0, distance)
+        totalHeight += distance
+        console.log(totalHeight)
+        console.log(scrollHeight)
+
+        if (totalHeight >= scrollHeight) {
+          clearInterval(timer)
+          resolve();
+          console.log("finish ... !")
+        }
+      }, 500)
+    })
+  })
+}
+
+let userList = []
+
+const getUserIdList = async (page) => {
+  await autoScroll(page)
+  // Get user id in Group members
+  const userIdList = await page.evaluate(async () => {
+    const userIdValue = document.querySelectorAll('table[id*="member_"]')
+    return Array.from(userIdValue).map(item => item.id.split("member_")[1])
+  })
+  userList.push(userIdList)
+
+  if (await page.$('div#m_more_item') !== null) {
+    await page.click('div#m_more_item a');
+    await getUserIdList(page)
+  }
+}
+
+async function main() {
+  const groupID = "1719912018231214" //2247849925503884
+  const url = "https://d.facebook.com/browse/group/members/?id=" + groupID
+  const cookie =
+    "sb=a6beXUxmABU9xS0KwkR3clpz; datr=bqbeXSqfBPU00FnuRWVrufnK; c_user=100005594357809; xs=37%3AgppwjlYVK9nNNQ%3A2%3A1574872721%3A5941%3A6351; fr=1CkYMcQ7QrlGloZLL.AWVp-p73xTlDw2MyWOUkofjMkg4.Bd3qZr.CE.AAA.0.0.Bd3qaR.AWUROxoR; spin=r.1001477064_b.trunk_t.1574872722_s.1_v.2_; wd=927x969; act=1574875261097%2F0; presence=EDvF3EtimeF1574875264EuserFA21B05594357809A2EstateFDt3F_5bDiFA2thread_3a1607618295944707A2EoF1EfF1C_5dEutc3F1574874678623G574875264955Elm3FnullCEchFDp_5f1B05594357809F1CC";
+  const browser = await puppeteer.launch({
+    headless: false
+  })
+  const page = await browser.newPage()
+  const cookieConverted = await convertCookieFacebook(cookie)
+  await page.setCookie(...cookieConverted)
+  await page.setViewport({
+    width: 1368,
+    height: 800
+  });
+  // await page.waitFor(1000);
+  await page.goto(url, {
+    waitUntil: "networkidle2"
+  });
+
+  await getUserIdList(page)
+  userList = userList.flat(1);
+
+  await browser.close();
+}
+
+module.exports = {
+  main
+}
