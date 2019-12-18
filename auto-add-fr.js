344bda7ae3a8e73ec2516862bf10d27fe2f18d1c
huyhohaui <b1.maitrongtoi@gmail.com>
2019-12-18 15:39:04 +0700

    up add fr

--- auto-add-fr.js
+++ auto-add-fr.js
@@ -6,12 +6,6 @@ const { convertCookieFacebook } = require("./cookie");
 
 async function clickAdd(page) {
   await page.click('a[href*="/a/mobile/friends/profile_add_friend"]');
-<<<<<<< HEAD
-
-  console.log("add success!");
-  console.log("AddFr Finish ...!");
-=======
->>>>>>> 8454a510dfc36550ba0462ab881a15b3a78ec8b0
 }
 
 // const clickAddFr = new CronJob("0 * * * * *", async () => {
@@ -35,12 +29,7 @@ async function clickAdd(page) {
 // }
 
 async function main() {
-<<<<<<< HEAD
   const userId = ["100043842166914", "100011521398663"];
-=======
-  // List user id in group
-  const userId = ["100043842166914"];
->>>>>>> 8454a510dfc36550ba0462ab881a15b3a78ec8b0
   let urlUser = "https://d.facebook.com/";
   const cookie =
     "sb=a6beXUxmABU9xS0KwkR3clpz; datr=bqbeXSqfBPU00FnuRWVrufnK; c_user=100005594357809; xs=37%3AgppwjlYVK9nNNQ%3A2%3A1574872721%3A5941%3A6351; fr=1CkYMcQ7QrlGloZLL.AWVp-p73xTlDw2MyWOUkofjMkg4.Bd3qZr.CE.AAA.0.0.Bd3qaR.AWUROxoR; spin=r.1001477064_b.trunk_t.1574872722_s.1_v.2_; wd=927x969; act=1574875261097%2F0; presence=EDvF3EtimeF1574875264EuserFA21B05594357809A2EstateFDt3F_5bDiFA2thread_3a1607618295944707A2EoF1EfF1C_5dEutc3F1574874678623G574875264955Elm3FnullCEchFDp_5f1B05594357809F1CC";
@@ -55,7 +44,6 @@ async function main() {
     width: 1368,
     height: 800
   });
-<<<<<<< HEAD
   // await page.waitFor(1000);
 
   for (index = 0; index < userId.length; index++) {
@@ -68,23 +56,6 @@ async function main() {
 
     console.log("Success ...!");
   }
-=======
-
-  // Auto click add friend after 1m
-  let indexUserId = -1;
-  const clickAddFr = new CronJob("0 * * * * *", async () => {
-    indexUserId++;
-    if (indexUserId < userId.length) {
-      await page.goto(urlUser + userId[indexUserId], {
-        waitUntil: "networkidle2"
-      });
-      await clickAdd(page);
-    } else {
-      return false;
-    }
-  });
-  clickAddFr.start();
->>>>>>> 8454a510dfc36550ba0462ab881a15b3a78ec8b0
 }
 
 main();
